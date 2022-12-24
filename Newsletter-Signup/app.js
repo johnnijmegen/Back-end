e0dba310f22d906bde96bcef59a723c0e393b835
johnnijmegen <johnnijmegen@hotmail.com>
2022-12-24 12:19:18 +0100

    added mailchimp

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,12 @@
+const bodyParser = require("body-parser");
+const https = require("https");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on 3000`);
+});
