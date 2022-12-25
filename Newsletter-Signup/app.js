791bc05180890770e748ce9431a2ff2a0857a3a2
johnnijmegen <johnnijmegen@hotmail.com>
2022-12-25 16:15:14 +0100

    addded backend

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,7 +1,6 @@
 const bodyParser = require("body-parser");
-const https = require("https");
 const express = require("express");
-const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -13,9 +12,28 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://${dc}.api.mailchimp.com/3.0/ping";
+
+  https.request(url, options, function (response) {});
 
   res.send(
     `Hello ${firstName} ${lastName}, thank you for subscribing to our newsletter.`
@@ -25,3 +43,9 @@ app.post("/", (req, res) => {
 app.listen(3000, () => {
   console.log(`Server is running on 3000`);
 });
+
+// API
+// ef0485767c67b7f6be10c7d12d51037f - us12;
+
+// ID
+// 5d58e71543
