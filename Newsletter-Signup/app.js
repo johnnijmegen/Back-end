268e4f1b818c81d99425f54e6a05dfd91a07f82d
johnnijmegen <johnnijmegen@hotmail.com>
2023-01-02 18:05:02 +0100

    added mailchimp

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,55 +1,81 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const https = require("https");
-const request = require("request");
-
-const app = express();
-
-// Allows use of local files
-app.use(express.static("public"));
-
-// Add bodyparser to app.
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/signup.html");
-});
-
-app.post("/", (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-        },
-      },
-    ],
-  };
-
-  // const jsonData = JSON.stringify(data);
-
-  // const url = `https://us12.admin.mailchimp.com/`;
-
-  https.request(url, options, function (response) {});
-
-  res.send(
-    `Hello ${firstName} ${lastName}, thank you for subscribing to our newsletter.`
-  );
-});
-
-app.listen(3000, () => {
-  console.log(`Server is running on 3000`);
-});
-
 // API
-// ef0485767c67b7f6be10c7d12d51037f - us12;
+//  ef0485767c67b7f6be10c7d12d51037f-us12;
 
 // ID
 // 5d58e71543
+
+//Requiring mailchimp's module
+//For this we need to install the npm module @mailchimp/mailchimp_marketing. To do that we write:
+//npm install @mailchimp/mailchimp_marketing
+
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+//Requiring express and body parser and initializing the constant "app"
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//Using bod-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//The public folder which holds the CSS
+
+app.use(express.static("public"));
+//Listening on port 3000 and if it goes well then logging a message saying that the server is running
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running at port 3000");
+});
+//Sending the signup.html file to the browser as soon as a request is made on localhost:3000
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+//Setting up MailChimp
+mailchimp.setConfig({
+  apiKey: "ef0485767c67b7f6be10c7d12d51037f-us12",
+  server: "us12",
+});
+
+//As soon as the sign in button is pressed execute this
+app.post("/", function (req, res) {
+  //*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
+  const firstName = req.body.firstName;
+  const secondName = req.body.secondName;
+  const email = req.body.email;
+
+  //*****************************ENTER YOU LIST ID HERE******************************
+  const listId = "5d58e71543";
+
+  //Creating an object with the users data
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: secondName,
+    email: email,
+  };
+
+  //Uploading the data to the server
+  async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+
+    //If all goes well logging the contact's id
+    res.sendFile(__dirname + "/success.html");
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+  }
+
+  //Running the function and catching the errors (if any)
+  // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
+  // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+  run().catch((e) => res.sendFile(__dirname + "/failure.html"));
+});
